09f93b95aeba0f071e43ed163b2852bb27df0dff
joanst <soce135@gmail.com>
2024-10-30 18:44:43 +0100

    llista ingredients

--- P1.py
+++ P1.py
@@ -15,11 +15,10 @@ headers = {
     "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/5\
     37.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36"
 }
-
+'''
 # Web a la q accedim
 #pagina="https://ca.wikipedia.org/wiki/La_guerra_de_les_gal%C3%A0xies"
 pagina="https://www.lapastaperalscatalans.cat/pasta/receptes"
-
 
 #obtenim la pàgina sencera i creem un objecte beautiful soup per treballar-hi
 page= requests.get(pagina, headers=headers)
@@ -27,15 +26,6 @@ soupPage= BeautifulSoup(page.content, features="html.parser")
 
 #Definim la llista per a desar els links de totes les receptes
 linksReceptes=[]
-
-#afegim tots els links de la primera pàgina de receptes en una llista
-receptes =soupPage.find_all("h2", class_="entry-title")
-for recepta in receptes :
-    #nomRecepta= recepta.get_text().strip("\n")
-    linkRecepta= recepta.find("a").get("href")
-    #print(nomRecepta)
-    #print(linkRecepta+"\n")
-    linksReceptes.append(linkRecepta)
 
 #Obtenir el numero de pàgines de receptes
 ultimaPagina= soupPage.find("a", class_="last")
@@ -55,6 +45,26 @@ for linkRecepta in linksReceptes:
     print(linkRecepta)
 
 print("Tenim "+str(len(linksReceptes))+" Receptes")
+
+'''
+
+
+#Explorem la pàgina de la recepta
+pagina= "https://www.lapastaperalscatalans.cat/receptes/peix/pasta-amb-carbassons-tonyina-llimona-i-perfum-dalfabrega-i-menta.html"
+
+page= requests.get(pagina, headers=headers)
+soupPage= BeautifulSoup(page.content, features="html.parser")
+
+#Obtenim el nom de la recepta
+nomRecepta =soupPage.find("h1", class_="entry-title")
+nomRecepta=nomRecepta.get_text()
+#Obtenim els ingredients
+ingredients =soupPage.find("div", class_="entry-content content") 
+ingredients = ingredients.find_all("li")
+llistaIngredients =[]
+for ingredient in ingredients:
+    llistaIngredients.append(ingredient.get_text())
+
 
 
 
@@ -77,4 +87,14 @@ linkRecepta= recepta.find("a")
 linkRecepta= linkRecepta.get("href")
 print("el link de la recepta és: "+linkRecepta)
 
+'''
+''' La primera pagina està inclosa dins el proper loop
+#afegim tots els links de la primera pàgina de receptes en una llista
+receptes =soupPage.find_all("h2", class_="entry-title")
+for recepta in receptes :
+    #nomRecepta= recepta.get_text().strip("\n")
+    linkRecepta= recepta.find("a").get("href")
+    #print(nomRecepta)
+    #print(linkRecepta+"\n")
+    linksReceptes.append(linkRecepta)
 '''
